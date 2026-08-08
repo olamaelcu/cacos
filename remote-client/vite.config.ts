6ff78e265d414c48d8ea80b520e3070d872bb6ed
Jacky Alcine <yo@jacky.wtf>
2026-08-07 21:05:11 -0400

    add tasks and config

--- remote-client/vite.config.ts
+++ remote-client/vite.config.ts
@@ -3,7 +3,10 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import mkcert from 'vite-plugin-mkcert';
 
 export default defineConfig({
-  plugins: [mkcert(), sveltekit()],
+  plugins: [mkcert({
+    force: true,
+    autoUpgrade: true,
+  }), sveltekit()],
   server: {
     port: 5194,
     strictPort: true,
